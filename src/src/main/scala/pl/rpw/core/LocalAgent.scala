472701490c0f77882a5d5199569c4e8a4da95e1d
Dawid Sitnik <sitnik.dawid1@gmail.com>
2020-05-24 21:27:55 +0200

    Add files via upload

---
+++ src/src/main/scala/pl/rpw/core/LocalAgent.scala
@@ -0,0 +1,197 @@
+package pl.rpw.core
+
+import java.io.{File, FileWriter}
+import java.time.LocalDateTime
+
+import akka.actor.Actor
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.regression.{GBTRegressionModel, GBTRegressor}
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.{SparkContext, sql}
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+class LocalAgent extends Actor{
+
+  //creating Spark object
+  private[this] val spark = SparkSession.builder().master("local[*]").getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+  val sparkContext: SparkContext = spark.sparkContext
+  val sqlContext: SQLContext = spark.sqlContext
+
+  val n_iterations = 5000 //number of iterations in which the prediction is made
+  val n_iterations_retrain = 1000 //number of iterations after which the model is refreshed
+  val historyFilePath = "./../../../../../data/history.txt" //filepath for time series of usage history
+
+  def receive = {
+    /**
+    create model basing on history loaded from file
+    generate values for sin(t) and predict values for t+1
+    at the end save new history usage to the file
+     */
+    case "start" =>
+      println("Starting new agent")
+      //loading history of data usage and training model
+      var model = this.prepareNewModel(historyFilePath)
+      var x : Int = 1;
+      var value : Double= 0.0d
+      var predictedVal: Double = 0.0d
+      val usageHistory = mutable.Stack(0.0d)
+      val predictedHistory = mutable.Stack(0.0d)
+
+      //working on a fake task which is calculating sin function
+      while(x<n_iterations){
+        //retrain the model on new data after n_iterations_retrain iterations
+        if (x%n_iterations_retrain == 0){
+          this.writeHistoryToFile(historyFilePath, usageHistory.reverse,true)
+          model = this.prepareNewModel(historyFilePath)
+        }
+        // generate value
+        value = math.sin(math.toRadians(x))
+
+        //predict value
+        predictedVal = this.predict(model, usageHistory)
+
+        predictedHistory.push(predictedVal)
+        usageHistory.push(value)
+
+        sender() ! predictedVal.toString
+        x=x+1
+      }
+      //saving real data usage and predictions to make visualizations of the process
+      this.writeHistoryToFile("/home/andy/Desktop/data/usage.txt", usageHistory, false)
+      this.writeHistoryToFile("/home/andy/Desktop/data/predicted.txt", predictedHistory, false)
+
+      println("done!")
+      sender() ! "The task is done!"
+
+  }
+  /**
+  pipeline for creating new GBT model
+  the model is trained on history saved in the file from filePath
+   */
+  private[this] def prepareNewModel(filePath: String): GBTRegressionModel ={
+    val history = this.loadTimeseriesFromTxt(filePath)
+    val training = this.prepareTrainingDataset(history)
+    this.createGradientBoostedTreeRegressionModel(training)
+  }
+  /**
+  writing stack to file
+  if append is set to true, the values are appended
+  if it is set to false the values are overeaten
+  */
+  private[this] def writeHistoryToFile(filePath: String, history: mutable.Stack[Double], append: Boolean){
+    val writer = new FileWriter(new File(filePath), append)
+    for(item <- history ){
+      writer.write(LocalDateTime.now().toString()+";"+item.toString()+'\n')
+    }
+    writer.close()
+  }
+  /**
+  use the model to predict value in t+1, basing on values from range t-n+1 : t
+   */
+  private[this] def predict(model: GBTRegressionModel, historyList: mutable.Stack[Double]):Double = {
+    //the model can be used only if the history is longer than the look back of the model
+    //if it is shorter we return the previous value
+    if(historyList.size < 12){
+        return historyList(0)
+      }
+
+    val values = List.concat(ListBuffer(0.0d), historyList.take(10))
+    import spark.implicits._
+    val values_df = List(values).map(x => (x(0), x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8), x(9), x(10))).toDF()
+
+    //creating features vector
+    val assembler = new VectorAssembler().
+      setInputCols(Array("_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9", "_10", "_11")).
+      setOutputCol("features")
+
+    val row = assembler.transform(values_df)
+    model.transform(row).select("prediction").collectAsList().get(0).getDouble(0)
+  }
+
+
+  /**
+  reshaping timeseries of shape 1 x n to m x n,
+  where m is the size of previous values which
+  are used as the attributes for the model
+   */
+  private[this] def temporize(valuesList:List[String]): ListBuffer[ListBuffer[Float]] = {
+    val output_X = new ListBuffer[ListBuffer[Float]]()
+    val step = 11
+    for( i <- 0 to valuesList.size - step - 1){
+      val t = new ListBuffer[Float]()
+      for (j <- 1 to step){
+        //Gather past records up to the step period
+        t.append(valuesList((i+j-1).toInt).toFloat)
+      }
+      output_X.append(t)
+    }
+    output_X
+  }
+
+  /**
+   loads timeseries data from txt and converts it to dataframe
+   */
+  private[this] def loadTimeseriesFromTxt(filePath: String): sql.DataFrame = {
+    var df = spark.read
+      .option("header", "false")
+      .option("sep", ";")
+      .csv(filePath)
+    val newNames = Seq("timestamp", "value")
+    df = df.toDF(newNames: _*)
+    df
+  }
+
+  /**
+   reshapes appends n past values to each record in dataframe
+   and transforms it to the form in which it can be used by the model
+   */
+  private[this] def prepareTrainingDataset(df: sql.DataFrame): sql.DataFrame = {
+    import spark.implicits._
+    var valueList = df.select("value").map(r => r.getString(0)).collect.toList
+
+    //reversing list to get the newest values first and limiting its length to 1000 elements
+    valueList = valueList.reverse.take(n_iterations_retrain)
+    //reshaping data
+    val temporized_list = this.temporize(valueList)
+
+    //converting list to data frame
+    val df_temporized = temporized_list.map(x => (x(0), x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8), x(9), x(10))).toDF()
+
+    //creating features vector
+    val assembler = new VectorAssembler().
+      setInputCols(Array("_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9", "_10", "_11")).
+      setOutputCol("features")
+    val training = assembler.transform(df_temporized)
+    training
+  }
+  /**
+  creates GBT model
+   */
+  private[this] def createGradientBoostedTreeRegressionModel(training: sql.DataFrame): GBTRegressionModel = {
+    val gbt = new GBTRegressor()
+    .setLabelCol("_1")
+    .setMaxIter(10)
+
+    // Train model. This also runs the indexer.
+    val model = gbt.fit(training)
+
+    // Make predictions.
+    val predictions = model.transform(training)
+
+    // Select (prediction, true label) and compute test error.
+    val evaluator = new RegressionEvaluator()
+    .setLabelCol("_1")
+    .setPredictionCol("prediction")
+    .setMetricName("rmse")
+
+    val rmse = evaluator.evaluate(predictions)
+    println(s"Model created. RMSE = $rmse")
+    model
+  }
+
+
+}
--- src/src/main/scala/pl/rpw/core/MainApp.scala
+++ src/src/main/scala/pl/rpw/core/MainApp.scala
@@ -1,8 +1,6 @@
 package pl.rpw.core
 
 import akka.actor.{ActorSystem, Props}
-import org.apache.spark.ml.regression.LinearRegression
-
 
 object MainApp extends App {
   val system = ActorSystem("HelloSystem")
@@ -13,27 +11,7 @@ object MainApp extends App {
   helloActor ! "I never loved you"
   helloActor ! "me liek cookiez"
 
-  // Load training data
-  val training = org.apache.spark.ml.regression.LinearRegression.read.format("libsvm")
-    .load("data/mllib/sample_linear_regression_data.txt")
-
-  val lr = new LinearRegression()
-    .setMaxIter(10)
-    .setRegParam(0.3)
-    .setElasticNetParam(0.8)
-
-  // Fit the model
-  val lrModel = lr.fit(training)
-
-  // Print the coefficients and intercept for linear regression
-  println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-
-  // Summarize the model over the training set and print out some metrics
-  val trainingSummary = lrModel.summary
-  println(s"numIterations: ${trainingSummary.totalIterations}")
-  println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
-  trainingSummary.residuals.show()
-  println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
-  println(s"r2: ${trainingSummary.r2}")
-
+	val system = ActorSystem("LocalAgent")
+  val localAgent = system.actorOf(Props[LocalAgent], name = "LocalAgent")
+  localAgent ! "start"
 }
