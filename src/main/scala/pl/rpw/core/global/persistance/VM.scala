213c3ec2adacd25895938d0ebae9aff37ca2d812
Delebrith <paulina.szwed.1996@gmail.com>
2020-05-31 19:14:31 +0200

    add persistance layer for GUA

--- src/main/scala/pl/rpw/core/global/persistance/VM.scala
+++ src/main/scala/pl/rpw/core/global/persistance/VM.scala
@@ -3,7 +3,7 @@ package pl.rpw.core.global.persistance
 import slick.lifted.Tag
 import slick.jdbc.H2Profile.api._
 
-class VM (tag: Tag) extends Table[(String, String, Int, Int, Int)](tag, "HIPERVISOR") {
+class VM (tag: Tag) extends Table[(String, String, Int, Int, Int)](tag, "VM") {
   def id = column[String]("ID", O.PrimaryKey, O.AutoInc) // This is the primary key column
   def state = column[String]("STATE")
   def cpu = column[Int]("CPU")
